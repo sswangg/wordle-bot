9f686a79dcf59223247ab3dd65ee023ebcf4fd7d
Sophia Wang <s.sophi.w@gmail.com>
2022-08-01 15:44:38 -0400

    Fixed

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from time import sleep
 from datetime import date
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_word():
@@ -15,12 +17,15 @@ def get_word():
     return words[days_from_cigar_day % 2309]
 
 
-browser = webdriver.Chrome()
-browser.get('https://www.powerlanguage.co.uk/wordle/')
+service = ChromeService(executable_path=ChromeDriverManager().install())
+
+browser = webdriver.Chrome(service=service)
+browser.get('https://www.nytimes.com/games/wordle/index.html')
+
+button = browser.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74")
+button.click()
 
 document = browser.find_element(By.TAG_NAME, 'html')
-document.click()
-
 document.send_keys(get_word())
 document.send_keys(Keys.ENTER)
 
